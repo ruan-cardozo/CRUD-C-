4d0a8e367579f6a64f398a674298b58c7ca5067b
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-07 20:00:33 -0300

    criar enums para marcas e modelos

---
+++ Enums/MarcasVeiculos.cs
@@ -0,0 +1,27 @@
+namespace SistemaDeVeiculos.Enums
+{
+    public enum MarcasVeiculos
+    {
+        Ford,
+        Toyota,
+        Honda,
+        Chevrolet,
+        Volkswagen,
+        Nissan,
+        BMW,
+        MercedesBenz,
+        Audi,
+        Hyundai,
+        Kia,
+        Subaru,
+        Tesla,
+        Fiat,
+        Jeep,
+        Mazda,
+        Volvo,
+        Peugeot,
+        Renault,
+        Outra 
+    }
+
+}
---
+++ Enums/ModelosVeiculos.cs
@@ -0,0 +1,22 @@
+namespace SistemaDeVeiculos.Enums
+{
+    public enum ModelosVeiculos
+    {
+        Sedan,
+        Hatch,
+        SUV,
+        Pickup,
+        Coupe,
+        Conversivel,
+        Van,
+        Caminhao,
+        Moto,
+        Outro,
+        Esportivo,
+        Ônibus,
+        MicroÔnibus,
+        Trator,
+        Quadriciclo,
+    }
+
+}
