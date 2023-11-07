a0e7e1341ae5525f136b03b109a6f22a47f6aa99
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-07 20:00:24 -0300

    fix veiculo model

--- Models/VeiculoModel.cs
+++ Models/VeiculoModel.cs
@@ -1,13 +1,15 @@
+using SistemaDeVeiculos.Enums;
+
 namespace SistemaDeVeiculos.Models
 {
 
     public class VeiculoModel
     {
         public int Id { get; set; }
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
+        public MarcasVeiculos Marca { get; set; }
+        public ModelosVeiculos Modelo { get; set; }
         public int AnoFab { get; set; }
         public decimal Km { get; set; }
-        public string ChassiNum { get; set; }
+        public string? ChassiNum { get; set; }
     }
 }
